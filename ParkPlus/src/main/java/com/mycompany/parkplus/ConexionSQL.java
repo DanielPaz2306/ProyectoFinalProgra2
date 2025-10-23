1ae8701a844a9b159e11a91f4b813760ba0e92ac
DanielPaz2306 <pchinchillapaz2306@gmail.com>
2025-10-23 10:37:10 -0600

    Conexion SQL Exitosa, awebo

--- ParkPlus/src/main/java/com/mycompany/parkplus/ConexionSQL.java
+++ ParkPlus/src/main/java/com/mycompany/parkplus/ConexionSQL.java
@@ -12,20 +12,18 @@ import java.sql.SQLException;
  * @author pchin
  */
 public class ConexionSQL {
+   private static final String URL =
+    "jdbc:sqlserver://PABLO\\SQLEXPRESS;databaseName=PARKPLUS;encrypt=false;integratedSecurity=true;";
     
-    private static final String URL = "jdbc:sqlserver://PABLO\\SQLEXPRESS:1433;databaseName=PARKPLUS;integratedSecurity=true;encrypt=false;";
-    private static final String USER = "PC-PUERTA"; // o el usuario que uses
-    private static final String PASSWORD = "12345678"; // cámbialo por tu clave real
-    
-    public static Connection conectar() {
-        Connection conexion = null;
-        try {
-            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("✅ Conexión exitosa a SQL Server");
-        } catch (SQLException e) {
-            System.out.println("❌ Error al conectar: " + e.getMessage());
-        }
-        return conexion;
+   public static Connection conectar() {
+    Connection conexion = null;
+    try {
+        conexion = DriverManager.getConnection(URL);
+        System.out.println("✅ Conexión exitosa a SQL Server");
+    } catch (SQLException e) {
+        System.out.println("❌ Error al conectar: " + e.getMessage());
     }
+    return conexion;
+}
     
 }
